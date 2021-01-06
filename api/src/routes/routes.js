3df281517e85c9a556759c46a22be6cb3210a19f
Michel Bernardo <bernardoenglish@outlook.com>
2021-01-06 17:08:15 -0400

    adding AnimalXBatchRegister controlllers and models

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -4,6 +4,7 @@ const routes = express.Router();
 const Person = require('./personRoutes');
 const Animal = require('./animalRoutes');
 const Batch = require('./batchRoutes');
+const AnimalBatchRegister = require('./animalBatchRoutes');
 
 routes.get('/', function (req, res) {
   res.send('Im working here');
@@ -12,5 +13,6 @@ routes.get('/', function (req, res) {
 Person(routes);
 Animal(routes);
 Batch(routes);
+AnimalBatchRegister(routes);
 
 module.exports = routes;
