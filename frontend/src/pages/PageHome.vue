01c789e05f6315b94d8358b44c0790679f13bf41
Michel Bernardo <bernardoenglish@outlook.com>
2021-01-08 16:38:11 -0400

    setting configurations into the home page input

--- frontend/src/pages/PageHome.vue
+++ frontend/src/pages/PageHome.vue
@@ -3,10 +3,25 @@
     <TheNavbar />
     <div class="main">
       <div class="d-flex justify-content-center">
-        <h1>Informações de registros</h1>
+        <h1>Pesquisar registros</h1>
       </div>
       <div class="input-box">
         <TheSearchInput />
+      </div>
+      <div>
+        <b-card
+          bg-variant="dark"
+          header="Nome do animal - Lote"
+          text-variant="white"
+          class="text-center"
+        >
+          <div class="d-flex flex-column align-items-start">
+            <b-card-text>Data de entrada: </b-card-text>
+            <b-card-text>Data de saída: </b-card-text>
+            <b-card-text>Última movimentação: </b-card-text>
+            <b-card-text>IC Bezerro: </b-card-text>
+          </div>
+        </b-card>
       </div>
     </div>
   </div>
